769a8cf9fc22928ef778dd179450f7baa716bb83
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-10-09 17:30:23 +0530

    Add files via upload

---
+++ Lecture2-CipherSchools/Lecture2.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // This line means Header
+using namespace std; // std makes sure that cout is a part of "iostream"
+int main()  // we have written a main "function"
+{
+    cout<<"Hello world";
+    return 0;
+}
